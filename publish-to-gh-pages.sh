3fbfb24766d74c5a0b0fe711a213500589772484
leeningzzu <leeningzzu@gmail.com>
2019-03-28 03:20:58 +0800

    new blog

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ev
+
+# get clone master
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+
+cd ../
+mv .deploy_git/.git/ ./public/
+
+cd ./public
+
+git config user.name "leeningzzu"
+git config user.email "leeningzzu@163.com"
+
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+
+# Github Pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
+
+
